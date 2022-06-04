8906f954fd6aa681736dc1949ed62a0ddc7e0df5
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-06-04 13:17:37 +0900

    add lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
 };
+
+// https://exerror.com/error-test-environment-jest-environment-jsdom-cannot-be-found-make-sure-the-testenvironment-configuration-option-points-to-an-existing-node-module/
