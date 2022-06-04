d114ac0b1186d9b780c2915bd2f79fc2ec6f7c9e
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-06-04 16:27:35 +0900

    invalidate coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -8,14 +8,14 @@ module.exports = {
     },
   },
   setupFilesAfterEnv: ["./src/jest.setup.ts"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
 };
 
 // https://exerror.com/error-test-environment-jest-environment-jsdom-cannot-be-found-make-sure-the-testenvironment-configuration-option-points-to-an-existing-node-module/
